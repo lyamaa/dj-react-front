0981ab380b902337d785eedaae0d0ac4f4af7ba5
surajmt8848 <suraj8848mt@gmail.com>
2021-01-05 20:31:06 +0545

    create container

---
+++ src/containers/Message.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Message = () => {
+    return (
+        <article class="message is-dark" style={{marginRight:"1.5rem"}}>
+            <div class="message-header">
+                <p className="title" style={{color: "whitesmoke"}}>Activate Your Account</p>
+                
+            </div>
+            <div class="message-body">
+              <p className="subtitle">  <strong>We have send you a Activation Link. Please visit yor email...</strong></p>
+  </div>
+        </article>
+    )
+}
+
+export default Message
